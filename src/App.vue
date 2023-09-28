81ac0b1436b25dd8833f075a492bcb63eed2f978
Huilang <admin@huilang.me>
2023-09-28 23:11:56 +0800

    添加自动获取json的功能
    Signed-off-by: Huilang <admin@huilang.me>

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
   <div v-loading="loading">
     <meta name="referrer" content="never">
-    <!-- <el-input id="data" v-model="data" :rows="4" type="textarea" placeholder="Please input" @change="dataChange" /> -->
+    <div v-if="showGetJson" class="get">
+      <el-select allow-create v-model="houseId" filterable placeholder="房屋id" @change="houseIdChange">
+        <el-option key="%252Fl3c2113333184394102" value="%252Fl3c2113333184394102" label="万科幸福誉"></el-option>
+        <el-option key="%252Fpanyuguangchang%252Fbp150ep400" value="%252Fpanyuguangchang%252Fbp150ep400" label="番禺广场"></el-option>
+        <el-option key="%2Fc2113327812276997" value="%2Fc2113327812276997" label="时代天韵"></el-option>
+        <el-option key="%2Fc2120077370349227" value="%2Fc2120077370349227" label="绿地城"></el-option>
+      </el-select>
+      <el-input-number v-model="housePage" :min="1" :max="10" label="页码"></el-input-number>
+      <el-switch v-model="getAll"></el-switch>
+      <el-button type="primary" round @click="getJson">获取当前数据</el-button>
+      <el-button class="copy" type="primary" round :data-clipboard-text="JSON.stringify(copyJson)">复制json</el-button>
+    </div>
     <div class="compare">
-      <el-select v-model="area" placeholder="区域"  @change="areaChange">
+      <el-select v-model="area" placeholder="区域" @change="areaChange">
         <el-option label="知识城" value="zhishicheng"/>
         <el-option label="番禺广场" value="panyuguangchang"/>
       </el-select>
-      <el-select id="select" filterable clearable v-model="file" placeholder="Select" @change="fileChange">
+      <el-select id="select" filterable clearable v-model="file" placeholder="选择旧数据" @change="fileChange">
         <el-option v-for="file in files" :key="file" :label="file" :value="file" />
       </el-select>
-      <el-select id="select" filterable clearable v-model="file2" placeholder="Select">
+      <el-select id="select" filterable clearable v-model="file2" placeholder="选择新数据">
         <el-option v-for="file in files" :key="file" :label="file" :value="file" />
       </el-select>
       <el-button type="primary" round @click="compare">对比</el-button>
     </div>
-    <el-table :data="compareResult.length==0 ?table.filter(data => (!search || data.desc.includes(search)) && (!tag || data.tags.includes(tag))):compareResult.filter(data => !search || data.desc.includes(search))" stripe style="width: 100%" :default-sort="{ prop: 'total', order: 'ascending' }">
+    <el-table :data="compareResult.length==0 ?table.filter(data => (!search || data.desc.includes(search)) && (!tag || data.tags.includes(tag))):compareResult.filter(data => (!search || data.desc.includes(search)) && (!tag || data.tags.includes(tag)))" stripe style="width: 100%" :default-sort="{ prop: 'total', order: 'ascending' }">
       <el-table-column prop="id" sortable label="编号" width="70" />
       <el-table-column label="图片" width="60">
         <template #default="scope">
@@ -55,12 +66,17 @@
 
 <script>
 const axios = require('axios');
+import ClipboardJS from 'clipboard'
 
 export default {
   name: 'App',
   data() {
     return {
       loading: true,
+      showGetJson: false,
+      getAll: true,
+      houseId: '%252Fl3c2113333184394102',
+      housePage: 1,
       table: [],
       file: '',
       file2: '',
@@ -70,10 +86,19 @@ export default {
       tag: '',
       tagDropdown: ['满两年','满五年'],
       area: 'zhishicheng',
-      compareResult: []
+      compareResult: [],
+      copyJson: {}
     }
   },
   methods: {
+    getUrlVar(url, key) {
+      var result = new RegExp("[?|&]" + key + "=([^&]*)", "i").exec(url); return result && unescape(result[1]) || "";
+    },
+    houseIdChange(){
+      this.table = [];
+      this.copyJson = {};
+      this.housePage = 1;
+    },
     areaChange(data){
       this.getFiles(data);
     },
@@ -165,10 +190,107 @@ export default {
         $this.loading = false
       });
     },
+    getJson(){
+      const $this = this;
+      const houseId = this.houseId;
+      if(houseId == ''){
+        this.$message({
+          showClose: true,
+          message: '请选择房屋',
+          type: 'error'
+        });
+        return;
+      }
+      const housePage = this.housePage;
+      $this.$message({
+        showClose: true,
+        duration: 600,
+        offset: 100,
+        message: '正在获取第'+housePage+'页数据'
+      });
+
+      $this.loading = true;
+      this.compareResult = [];
+      if($this.housePage == 1 || $this.getAll == false) $this.table = [];
+      axios.get('http://tao.xgzuche.com/house/json/getCurl.php?url=' + encodeURIComponent('https://m.ke.com/liverpool/api/ershoufang/getList?cityId=440100&condition=' + houseId + '&curPage=' + housePage))
+      .then(function (response) {
+        if(response.data.status == 1){
+          const data = response.data.data;
+          const list = data.data.data.getErShouFangList.list
+          if(list.length == 0){
+            if($this.getAll && $this.housePage != 1){
+              $this.$message({
+                showClose: true,
+                message: '获取完毕 共获取到' + $this.table.length + '个数据',
+                type: 'success'
+              });
+            }else{
+              $this.$message({
+                showClose: true,
+                message: '获取失败',
+                type: 'error'
+              });
+            }
+          }else{
+            let arr = [];
+            for (let i = 0; i < list.length; i++) {
+              const obj = list[i];
+              let tmp = {
+                "id": obj.houseCode,
+                "link": obj.jumpUrl,
+                "img": obj.listPictureUrl,
+                "title": obj.title,
+                "desc": obj.desc,
+                "total": obj.totalPrice,
+                "unit": obj.unitPrice.replace('元/平','')
+              };
+              arr.push(tmp);
+            }
+            $this.table = $this.table.concat(arr);
+            let now = new Date();
+            $this.copyJson = {
+              'data' : $this.table,
+              'date' : now.getFullYear() + "-" +((now.getMonth()+1)<10?"0":"")+(now.getMonth()+1)+"-"+(now.getDate()<10?"0":"")+now.getDate()
+            };
+            if($this.getAll){
+              $this.housePage++;
+              $this.getJson();
+            }
+          }
+        }else{
+          $this.$message({
+            showClose: true,
+            message: '获取失败',
+            type: 'error'
+          });
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {
+        $this.loading = false
+      });
+    }
   },
   mounted() {
+    const $this = this;
+    this.showGetJson = this.getUrlVar(window.location.href,'debug');
     this.getFiles(this.area)
-  }
+    this.clipboard = new ClipboardJS('.copy')
+    this.clipboard.on('success', function() {
+      $this.$message({
+        showClose: true,
+        message: '复制成功',
+        duration: 600,
+        type: 'success'
+      });
+    })
+  },
+  unmounted() {
+    // 销毁Clipboard实例，避免在其它页面或组件中实例化Clipboard后造成再次监听，产生重复回调
+    this.clipboard.destroy()
+  },
 }
 </script>
 
@@ -192,6 +314,11 @@ body{
 }
 #data{
   margin-bottom: 20px;
+}
+.get{
+  margin-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
 }
 .compare{
   display: flex;
